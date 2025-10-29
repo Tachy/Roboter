c0820a3eb32c5ebb2962deb81cf91c696aef07f2
Johannes Güntert <johannes@gutshof-guentert.de>
2025-10-29 11:49:04 +0100

    feat: Add GPIO23 shutdown button handler and service for controlled shutdown

---
+++ unkrautroboter_bilderkennung/systemd/gpio23-shutdown.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from gpiozero import Button  # type: ignore
+from signal import pause
+import os
+
+# GPIO3 (Pin 5) wird als Shutdown-Taster verwendet
+shutdown_btn = Button(23, pull_up=True, hold_time=1)  # 1 Sekunde halten für Shutdown
+
+
+def shutdown():
+    os.system("sudo shutdown -h now")
+
+
+shutdown_btn.when_held = shutdown
+
+pause()
